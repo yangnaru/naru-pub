ea3e9c1bf3df84ef8c88e301f54d5d1bb116f930
Jihyeok Seo <ji@hyeok.org>
2025-04-12 10:52:22 +0900

    `experimental.serverComponentsExternalPackages` has been moved to `serverExternalPackages`. Please update your next.config.mjs file accordingly.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@node-rs/argon2"],
-  },
+  serverExternalPackages: ["@node-rs/argon2"],
 };
 
 export default withSentryConfig(
