43ae4a463d64aa91f6e031e98fb1986ccb107277
Jihyeok Seo <ji@hyeok.org>
2025-07-19 16:29:41 +0900

    [@sentry/nextjs] Could not find `onRequestError` hook in instrumentation file. This indicates outdated configuration of the Sentry SDK. Use `Sentry.captureRequestError` to instrument the `onRequestError` hook: https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#errors-from-nested-react-server-components

--- control-plane/src/instrumentation.ts
+++ control-plane/src/instrumentation.ts
@@ -1,9 +1,2 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('../sentry.server.config');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config');
-  }
-}
+import * as Sentry from "@sentry/nextjs";
+export const onRequestError = Sentry.captureRequestError;
